e9ae73e658939d9b8906e2fa78c0a928d76c9807
GST-Slava <gstslava@gmail.com>
2022-05-04 23:30:17 +0300

    add components + css

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from './Header.module.css';
+
+export const Header = () => {
+    return (
+        <header className={classes.header}>
+            <img src={require('./../img/header_img.jpg')} alt="header logo"/>
+            Social Network Samurai Way
+        </header>
+    )
+}
